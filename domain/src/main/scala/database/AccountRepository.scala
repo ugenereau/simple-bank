3aada2966a09ecbcd87469c7b358657ff024d205
Adrien Ysnel <adrien.ysnel@compte-nickel.fr>
2021-10-05 11:02:57 +0200

    Exercice 9: manage database error

--- domain/src/main/scala/database/AccountRepository.scala
+++ domain/src/main/scala/database/AccountRepository.scala
@@ -1,8 +1,9 @@
 package fr.fpe.school
 package database
 
+import database.error.AccountRepositoryError
 import model.Account
 
 trait AccountRepository[F[_]] {
-  def insert(name: String): F[Account]
+  def insert(name: String): F[Either[AccountRepositoryError, Account]]
 }
