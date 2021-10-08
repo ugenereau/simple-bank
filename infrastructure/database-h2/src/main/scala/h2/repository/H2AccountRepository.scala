b0f5614a0b5f7e61237795488998f73e1e918678
Adrien Ysnel <adrien.ysnel@compte-nickel.fr>
2021-10-08 14:39:12 +0200

    Exercice 7: add a database connection

---
+++ infrastructure/database-h2/src/main/scala/h2/repository/H2AccountRepository.scala
@@ -0,0 +1,28 @@
+package fr.fpe.school
+package h2.repository
+
+import database.AccountRepository
+import h2.implicits._
+import h2.query.AccountQueries
+import model.Account
+
+import cats.effect.IO
+import doobie.h2.implicits._
+import doobie.implicits._
+import doobie.util.transactor.Transactor
+
+import java.time.OffsetDateTime
+import java.util.UUID
+
+final class H2AccountRepository(transactor: Transactor[IO]) extends AccountRepository {
+  override def insert(name: String): Account =
+    AccountQueries
+      .insert(name)
+      .withUniqueGeneratedKeys[(UUID, OffsetDateTime)]("id", "creation_date")
+      .map { case (id, creationDate) =>
+        Account(id, name, creationDate)
+      }
+      .transact(transactor)
+      .unsafeRunSync()
+
+}
